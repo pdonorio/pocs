6b026e53c3bb1183ff87ca9fb142683ffa95e633
pdonorio <tempo.pause@gmail.com>
2016-10-11 23:46:46 +0200

    Fixing problems with flask logs in uwsgi case

---
+++ wsgi/app/app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+For debugging purpose
+"""
+
+from myflask import app
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
--- wsgi/app/myflask.py
+++ wsgi/app/myflask.py
@@ -1,11 +1,42 @@
 # -*- coding: utf-8 -*-
 
+"""
+A basic app to test Flask behind uwsgi in production
+"""
+
+##########################
 from flask import Flask
+import logging
+import logging.handlers
+import sys
+
+##########################
+api_prefix = '/api'
+
+loglevel = logging.DEBUG
+myformat = logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+)
+# formatter = logging.Formatter(
+#     "%(asctime)s %(levelname)-21s @%(name)s %(message)s")
+
 app = Flask('poc')
 
-API_PREFIX = '/api'
+##########################
+# Only if using uwsgi add the file handler
+is_uwsgi = "uwsgi" in sys.modules.keys()
+if is_uwsgi:
+    # http://flask.pocoo.org/docs/0.11/errorhandling/
+    file_handler = logging.handlers.TimedRotatingFileHandler(
+        filename='/var/log/uwsgi/flask.log')
+    file_handler.setLevel(loglevel)
+    file_handler.setFormatter(myformat)
+    app.logger.addHandler(file_handler)
 
 
-@app.route(API_PREFIX + '/hello')
+##########################
+@app.route(api_prefix + '/hello')
 def hello():
+    app.logger.warning("Received request")
     return "Hello, World!"
